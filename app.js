4116ca4588df4d6dec35fb566bfd55a65b949de8
Ritik Negi <negiritik47@gmail.com>
2025-08-18 15:25:36 +0530

    env & zod

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const app = express();
 app.get("/", (req,res)=>
 res.send ("Hello"));
 
-const PORT = 3000;
+
+app.get("/home", (req,res)=>
+res.send ("Home"));
+
+
+app.get("/about", (req,res)=>
+res.send ("About"));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log(`Server is running at port:${PORT}`);
 });
---
+++ env.js
@@ -0,0 +1,19 @@
+import {z, ZodError} from "zod";
+
+// const ageScheme = z.number().min(18).max(100).int();
+// const userAge = 17;
+
+// try{
+// const parseUserAge = ageScheme.parse(userAge);
+// console.log(parseUserAge)
+// } catch(error){
+//     if(error instanceof ZodError){
+//         console.log(error.issues[0].message);
+//     } else{
+//         console.log("Unexpected error:", error);
+        
+//     }
+// }
+
+const portSchema = z.coerce.number.min(1).max(6553).default(3000);
+const PORT = portSchema.parse(process.env.PORT)
